b1c2f9fb24cedc57ada7aad59753eb05cb93f01e
chinju2005 <chinju.lorance2005@gmail.com>
2020-04-19 00:01:57 +0530

    secprogram

---
+++ secproram.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter the value:";
+    cin>>a;
+    cout<<"The value is:"<<a;
+
+    return 0;
+}
