2d27141dee1d1b80cf2b9c8fa0072210e0ffdebc
chinju2005 <chinju.lorance2005@gmail.com>
2020-04-19 05:24:29 +0530

    saved changes

---
+++ pattern1.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code inhis editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<j;
+         }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
