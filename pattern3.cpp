37fbc6816de040ab152efb574f28ec64b719be69
chinju2005 <chinju.lorance2005@gmail.com>
2020-04-19 05:48:46 +0530

    reverse star pattern

---
+++ pattern3.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code inhis editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(i=5;i>0;i--)
+    {
+        for(j=0;j<i;j++)
+        {
+        cout<<"*";
+       
+        }
+     cout<<endl;
+        
+    }
+    
+
+    return 0;
+}
