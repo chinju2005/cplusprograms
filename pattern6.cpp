70f9c375ef2eb8c9f6c0b81abde8e857ac829ee9
chinju2005 <chinju.lorance2005@gmail.com>
2020-04-19 06:29:05 +0530

    reverse character pattern

---
+++ pattern6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+    for(char i='d';i>='a';i--)
+    {
+        for(char j='a';j<='d';j++)
+        {
+        cout<<i;
+       
+        }
+     cout<<endl;
+    
+    }
+ return 0;
+}
